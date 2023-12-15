b0290edd5329d2b5f3e1f1bf480b7e965c43e00b
Palotás M. Péter <palotas@edu.bme.hu>
2023-12-15 11:28:11 +0100

    Add parallelogram drawing

--- parallelogram/scripts/main.js
+++ parallelogram/scripts/main.js
@@ -1,25 +1,61 @@
 function calculateByHeight() {
-	const b = Number(document.getElementById('base').value);
-	const h = Number(document.getElementById('height').value);
-	try {
-		document.getElementById('area1').value = parallalelogramAreaByBase(b, h);
-	} catch (error) {
-		document.getElementById('area1').value = error;
-	}
+    const b = Number(document.getElementById('base').value);
+    const h = Number(document.getElementById('height').value);
+    try {
+        document.getElementById('area1').value = parallalelogramAreaByBase(b, h);
+
+        var canvas = document.getElementById('by-base');
+        var ctx = canvas.getContext('2d');
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        const tilt = 20;
+        const offset = 70;
+
+        ctx.beginPath();
+        ctx.moveTo(offset, offset);
+        ctx.lineTo(b + offset, offset);
+        ctx.lineTo(b + tilt + offset, h + offset);
+        ctx.lineTo(tilt + offset, h + offset);
+        ctx.closePath();
+        ctx.fillStyle = 'white';
+        ctx.strokestyle = 'black';
+        ctx.stroke();
+    } catch (error) {
+        document.getElementById('area1').value = error;
+    }
 }
 
 function calculateByAngle() {
-	const a = Number(document.getElementById('side-a').value);
-	const b = Number(document.getElementById('side-b').value);
-	const alpha = Number(document.getElementById('angle').value);
-	try {
-		document.getElementById('area2').value = parallalelogramAreaBySides(a, b, alpha);
-	} catch (error) {
-		document.getElementById('area2').value = error;
-	}
+    const a = Number(document.getElementById('side-a').value);
+    const b = Number(document.getElementById('side-b').value);
+    const alpha = Number(document.getElementById('angle').value);
+    try {
+        document.getElementById('area2').value = parallalelogramAreaBySides(a, b, alpha);
+
+        var canvas = document.getElementById('by-angle');
+        var ctx = canvas.getContext('2d');
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+		const tilt = b * Math.cos(alpha * (Math.PI / 180));
+		const h = b * Math.sin(alpha * (Math.PI / 180));
+		
+        const offset = 70;
+
+        ctx.beginPath();
+        ctx.moveTo(offset, offset);
+        ctx.lineTo(a + offset, offset);
+        ctx.lineTo(a + offset + tilt,  h + offset);
+        ctx.lineTo(tilt + offset, h + offset);
+        ctx.closePath();
+        ctx.fillStyle = 'white';
+        ctx.strokestyle = 'black';
+        ctx.stroke();
+    } catch (error) {
+        document.getElementById('area2').value = error;
+    }
 }
 
 document.body.onload = () => {
-	calculateByHeight();
-	calculateByAngle();
+    calculateByHeight();
+    calculateByAngle();
 };
